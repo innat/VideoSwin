2b43b590224e4170233af1f3637297aa9edb4fe2
innat <innat.dev@gmail.com>
2023-10-03 18:25:22 +0600

    add model config

---
+++ configs/swin_tiny_patch244_window877_kinetics400_1k.py
@@ -0,0 +1,97 @@
+
+model=dict(
+    backbone=dict(
+        patch_size=(2,4,4), 
+        drop_path_rate=0.1
+    ), 
+    test_cfg=dict(max_testing_views=4)
+)
+
+# dataset settings
+dataset_type = 'VideoDataset'
+data_root = 'data/kinetics400/train'
+data_root_val = 'data/kinetics400/val'
+ann_file_train = 'data/kinetics400/kinetics400_train_list.txt'
+ann_file_val = 'data/kinetics400/kinetics400_val_list.txt'
+ann_file_test = 'data/kinetics400/kinetics400_val_list.txt'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], 
+    std=[58.395, 57.12, 57.375], 
+)
+
+train_pipeline = [
+    dict(type='DecordInit'),
+    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandomResizedCrop'),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NTHWC'),
+]
+
+val_pipeline = [
+    dict(type='DecordInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=32,
+        frame_interval=2,
+        num_clips=1,
+        test_mode=True
+    ),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='Flip', flip_ratio=0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NTHWC'),
+]
+
+test_pipeline = [
+    dict(type='DecordInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=32,
+        frame_interval=2,
+        num_clips=4,
+        test_mode=True
+    ),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 224)),
+    dict(type='ThreeCrop', crop_size=224),
+    dict(type='Flip', flip_ratio=0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NTHWC'),
+]
+
+evaluation = dict(
+    interval=5, 
+    metrics=['top_k_accuracy', 'mean_class_accuracy']
+)
+
+# optimizer
+optimizer = dict(
+    type='AdamW', 
+    lr=1e-3, 
+    betas=(0.9, 0.999), 
+    weight_decay=0.02,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'backbone': dict(lr_mult=0.1)}
+            )
+    )
+
+# learning policy
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=0,
+    warmup='linear',
+    warmup_by_epoch=True,
+    warmup_iters=2.5
+)
+total_epochs = 30
