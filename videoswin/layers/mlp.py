9b31002cc388beb3881e086de7fde0a1808b07c0
innat <innat.dev@gamil.com>
2023-12-30 17:22:56 +0600

    update mlp: update with new api

--- videoswin/layers/mlp.py
+++ videoswin/layers/mlp.py
@@ -1,7 +1,7 @@
 
 from keras import layers
 
-class Mlp(layers.Layer):
+class MLP(layers.Layer):
     """ Multilayer perceptron."""
     
     def __init__(
@@ -9,22 +9,25 @@ class Mlp(layers.Layer):
         in_features,
         hidden_features=None,
         out_features=None,
-        drop=0.0,
+        drop_rate=0.0,
         act_layer=layers.Activation("gelu"),
         **kwargs
     ):
         super().__init__(**kwargs)
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = layers.Dense(hidden_features)
-        self.fc2 = layers.Dense(out_features)
+        self.out_features = out_features or in_features
+        self.hidden_features = hidden_features or in_features
+        self.drop_rate = drop_rate
         self.act = act_layer
-        self.drop = layers.Dropout(drop)
+        
+    def build(self, input_shape):
+        self.fc1 = layers.Dense(self.hidden_features)
+        self.fc2 = layers.Dense(self.out_features)
+        self.dropout = layers.Dropout(self.drop_rate)
 
     def call(self, x, training=None):
         x = self.fc1(x)
         x = self.act(x)
-        x = self.drop(x, training=training)
+        x = self.dropout(x, training=training)
         x = self.fc2(x)
-        x = self.drop(x, training=training)
+        x = self.dropout(x, training=training)
         return x
