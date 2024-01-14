2d80751243ff9d7b71d33608b1a113ed9a7fcec4
innat <innat.dev@gamil.com>
2024-01-14 18:03:38 +0600

    🔨 update python version

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
