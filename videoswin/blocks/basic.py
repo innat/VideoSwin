a28aa61ddca1c7b1628319e0c05027854668be97
innat <innat.dev@gmail.com>
2024-03-22 02:05:07 +0600

    replace tf.tensorshape to keras.kerastensor

--- videoswin/blocks/basic.py
+++ videoswin/blocks/basic.py
@@ -118,7 +118,7 @@ class VideoSwinBasicLayer(keras.Model):
         if self.downsample is not None:
             # TODO: remove tensorflow dependencies.
             # GitHub issue: https://github.com/keras-team/keras/issues/19259 # noqa: E501
-            output_shape = tf.TensorShape(
+            output_shape = keras.KerasTensor(
                 [
                     input_shape[0],
                     self.depth_pad,
