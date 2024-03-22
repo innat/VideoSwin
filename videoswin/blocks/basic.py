4bf9d9b2cc19bab184c5498527d6a8f1267a44cd
innat <innat.dev@gmail.com>
2024-03-22 15:27:46 +0600

    🎨 can it take non square input

--- videoswin/blocks/basic.py
+++ videoswin/blocks/basic.py
@@ -114,25 +114,6 @@ class VideoSwinBasicLayer(keras.Model):
 
         self.built = True
 
-    # def compute_output_shape(self, input_shape):
-    #     if self.downsample is not None:
-    #         # TODO: remove tensorflow dependencies.
-    #         # GitHub issue: https://github.com/keras-team/keras/issues/19259 # noqa: E501
-    #         # output_shape = tf.TensorShape(
-    #         #     [
-    #         #         input_shape[0],
-    #         #         self.depth_pad,
-    #         #         self.height_pad // 2,
-    #         #         self.width_pad // 2,
-    #         #         2 * self.input_dim,
-    #         #     ]
-    #         # )
-
-    #         output_shape = (input_shape[0], ) + (self.depth_pad, ) + (self.height_pad // 2, ) + (self.width_pad // 2, ) + (self.input_dim, )
-    #         return output_shape
-
-    #     return input_shape
-
     def call(self, x, training=None):
         input_shape = ops.shape(x)
         batch_size, depth, height, width, channel = (
